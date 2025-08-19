5637f9ce38af3b833f050a8fbb0fe95c085f467f
hassaan141 <m2farooq@uwaterloo.ca>
2025-08-19 19:07:35 -0400

    Add my-expo-app as regular folder

---
+++ my-expo-app/babel.config.js
@@ -0,0 +1,22 @@
+// module.exports = function (api) {
+//   api.cache(true);
+//   let plugins = [];
+
+//   return {
+//     presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
+
+//     plugins,
+//   };
+// };
+
+module.exports = function (api) {
+  api.cache(true);
+  let plugins = [
+    ["module:react-native-dotenv", { moduleName: "@env", path: ".env" }]
+  ];
+
+  return {
+    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
+    plugins,
+  };
+};
